6ed6bef08a1f2c7560f805d0bb72676affcd15b7
Michael Prather <mprather1@gmail.com>
2016-05-03 17:03:20 -0500

    added -y to repositories

--- install.sh
+++ install.sh
@@ -23,12 +23,12 @@ read -p "${prompt}" update_software
 case $update_software in
   "1")
     sudo add-apt-repository ppa:webupd8team/atom -y
-    sudo add-apt-repository ppa:chris-lea/node.js
+    sudo add-apt-repository ppa:chris-lea/node.js -y
     sudo apt-get update && sudo apt-get install -y openssh-server avahi-daemon clementine ftp ftpd autofs sshfs libreoffice lolcat cmatrix sl puddletag deluge keepass2 geany vlc samba soundconverter ubuntu-restricted-extras fortunes fortunes-off gimp agave steam thunderbird remmina virtualbox calibre gparted curl libsqlite3-dev git vim postgresql git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libpq-dev libffi-dev libpq-dev pv toilet rig libaa-bin espeak nodjs && sudo apt-get upgrade -y
     sudo /usr/share/doc/libdvdread4/install-css.sh
     ;;
   "2")
-    sudo add-apt-repository ppa:chris-lea/node.js
+    sudo add-apt-repository ppa:chris-lea/node.js -y
     sudo apt-get update && sudo apt-get install -y cmatrix sl lolcat fortunes fortunes-off curl git openssh-server avahi-daemon autofs sshfs vim postgresql git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev build-essential libpq-dev pv toilet rig libaa-bin nodejs && sudo apt-get upgrade -y
     echo "Done!!!"
     ;;
