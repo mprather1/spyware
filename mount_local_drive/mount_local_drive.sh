5238e4eff7466db6ea041571796c4726ff129457
Michael Prather <mprather1@gmail.com>
2016-05-04 17:23:26 -0500

    simplified mount local drive

--- mount_local_drive/mount_local_drive.sh
+++ mount_local_drive/mount_local_drive.sh
@@ -3,40 +3,15 @@ user=$(whoami)
 user_id=$(id -u $user)
 group_id=$(id -g $user)
 
-echo "Important Read this!!"
-echo "Be very careful!! Do not make mistakes!!
-Only use this for drives that you do not intend to remove!!"
-echo "Press any key to continue or CTRL+C to exit!!"
-read -n 1
+#argument 1 disk location
+#argument 2 mount point
+#argument 3 type
 
-echo "
-Please enter location of hard disk..."
-echo "Example: /dev/sda"
-read -e -p ${prompt} disk_location
-
-echo "
-Enter mount point..."
-echo "Example: /media/shared"
-read -e -p ${prompt} mount_point
-
-echo "
-Choose type..."
-echo "1.) ntfs 2.) vfat 3.) ext4"
-read type
-case $type in
-  '1') type="ntfs"
-  ;;
-  '2') type="vfat"
-  ;;
-  '3') type='ext4'
-  ;;
-esac
-
-sudo mkdir $mount_point && printf "${disk_location} ${mount_point} ${type} rw,auto,user,uid=${user_id},git=${group_id},fmask=0111,dmask=0000 0 0\n" | sudo tee -a /etc/fstab
+sudo mkdir $2 && printf "${1} ${2} ${3} rw,auto,user,uid=${user_id},git=${group_id},fmask=0111,dmask=0000 0 0\n" | sudo tee -a /etc/fstab
 
 sudo mount -a
 
 echo "All done!!"
-echo "If no errors have occured, hard disk is mounted in ${mount_point}!!"
+echo "If no errors have occured, hard disk is mounted in ${1}!!"
 echo "
 Go fuck yourself!!"
