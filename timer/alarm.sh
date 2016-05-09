1dbf4a1914fae6556f273f4de5213f7ae17e6e5c
Michael Prather <mprather1@gmail.com>
2016-05-09 00:08:04 -0500

    timer and alarm

--- timer/alarm.sh
+++ timer/alarm.sh
@@ -7,4 +7,4 @@ while [ "$date1" -ge `date +%s` ]; do
   echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r";
 done
 
-mplayer /usr/share/sounds/freedesktop/stereo/complete.oga 
+mplayer /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
