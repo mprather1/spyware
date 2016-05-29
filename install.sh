b3ed51b469d3920171543db16cae34b3b12d04db
Michael Prather <mprather1@gmail.com>
2016-05-29 00:28:47 -0500

    fixed a few things

--- install.sh
+++ install.sh
@@ -23,16 +23,18 @@ read -p "${prompt}" update_software
 case $update_software in
   "1")
     echo "Installing Ubuntu Desktop software..."
+    sudo apt-get update && sudo apt-get install -y curl
     sudo add-apt-repository -y ppa:webupd8team/atom
     curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-    sudo apt-get update && sudo apt-get install -y openssh-server avahi-daemon clementine ftp ftpd autofs sshfs libreoffice lolcat cmatrix sl puddletag deluge keepass2 geany vlc samba soundconverter fortunes fortunes-off gimp agave steam thunderbird remmina virtualbox calibre gparted libdvdread4 curl libsqlite3-dev git vim postgresql git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libpq-dev libffi-dev libpq-dev pv toilet rig libaa-bin espeak chromium-browser eclipse tuxguitar handbrake dosbox putty putty-tools unzip python-pip nmap atom mplayer nodejs && sudo apt-get upgrade -y
+    sudo apt-get update && sudo apt-get install -y openssh-server avahi-daemon clementine ftp ftpd autofs sshfs libreoffice lolcat cmatrix sl puddletag deluge keepass2 geany vlc samba soundconverter fortunes fortunes-off gimp agave steam thunderbird remmina virtualbox calibre gparted libdvdread4 libsqlite3-dev git vim postgresql git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libpq-dev libffi-dev libpq-dev pv toilet rig libaa-bin espeak chromium-browser eclipse tuxguitar handbrake dosbox putty putty-tools unzip python-pip nmap atom mplayer nodejs && sudo apt-get upgrade -y
     sudo /usr/share/doc/libdvdread4/install-css.sh
     echo "Done"
     ;;
   "2")
     echo "Installing Ubuntu Server software..."
+    sudo apt-get update && sudo apt-get install -y curl
     curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-    sudo apt-get update && sudo apt-get install -y cmatrix sl lolcat fortunes fortunes-off curl git openssh-server avahi-daemon autofs sshfs vim postgresql git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev build-essential libpq-dev pv toilet rig libaa-bin nodejs unzip python-pip nmap nodejs && sudo apt-get upgrade -y
+    sudo apt-get update && sudo apt-get install -y cmatrix sl lolcat fortunes fortunes-off git openssh-server avahi-daemon autofs sshfs vim postgresql git-core curl zlib1g-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev build-essential libpq-dev pv toilet rig libaa-bin nodejs unzip python-pip nmap nodejs && sudo apt-get upgrade -y
     touch installed.txt
     echo "Done!!!"
     ;;
@@ -160,12 +162,12 @@ if [ -f /usr/bin/xinput ]; then
   for id in $(xinput --list | \
             sed -n '/Kingsis Peripherals  Evoluent VerticalMouse 3.*pointer/s/.*=\([0-9]\+\).*/\1/p')
   do
-    if [ ! -z ${id+x}; then
+    if [ ! -z ${id+x} ]; then
         echo "Configuring Kingsis Peripherals Evoluent VerticalMouse 3..."
         sleep 1
         button_map="xinput set-button-map ${id} 1 2 3 4 5 6 7 9 8"
         touch ~/.xsessionrc
-        echo "#!/usr/bin/env bash\n\n${button_map}" > ~/.xsessionrc
+        printf "#!/usr/bin/env bash\n\n${button_map}" > ~/.xsessionrc
       else
         echo "Evoluent VerticalMouse not found, skipping mouse configuration..."
         sleep 1
@@ -185,9 +187,6 @@ sudo cp -r wallpaper/* /usr/share/xfce4/backdrops
 
 touch ~/.gtkrc-2.0
 cat gtkrc.txt > ~/.gtkrc-2.0
-
-touch ~/.config/menus/xfce-applications.menu
-cat menu/xfce-applications.txt > ~/.config/menus/xfce-applications.menu
 
 echo "Done"
 
