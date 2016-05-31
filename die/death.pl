fe2c977ebc6553b0d9e2f5267c091f5284f68ab8
Michael Prather <mprather1@gmail.com>
2016-05-30 23:27:35 -0500

    death function still needs work

--- die/death.pl
+++ die/death.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
-system('bash shutdown.sh');
+
+system("bash $current_directory/die/shutdown.sh");
 system("ssh mike\@notebook.local sudo shutdown -h now");
 system("sudo shutdown -h now");
