4c813fe04f0cdb3164ff868346990042df873c1a
Michael Prather <mprather1@gmail.com>
2016-06-04 22:21:43 -0500

    minor fixes

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ if [ ! -f ~/.bash_aliases ]
     for c in "${!scripts[@]}"; do
       directory=$(printf "%s" "${scripts[$c]}")
       printf "alias %s='bash ${current_directory}%s'\n" "$c" "${scripts[$c]}" >> ~/.bash_aliases
-      printf "${shebang}\ncurrent_directory=${current_directory}\n\n" | cat - "${current_directory}/${directory}" > temp && mv temp "${current_directory}/${directory}"
+      printf "${shebang}\ncurrent_directory=${current_directory}\n\n" | cat - "${current_directory}${directory}" > temp && mv temp "${current_directory}${directory}"
     done
     printf "alias death='perl ${current_directory}/die/death.pl'" >> ~/.bash_aliases
     echo "Done!!"
