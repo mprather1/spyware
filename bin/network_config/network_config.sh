6f67b91d0b91ecd1206ca7e69e29d2f6e5c88e01
Michael Prather <mprather1@gmail.com>
2016-06-11 03:05:17 -0500

    removed network.txt

--- bin/network_config/network_config.sh
+++ bin/network_config/network_config.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 source $(dirname $0)/../../current_directory.sh
 
-sudo sh -c "cat $(directory)/network.txt > /etc/network/interfaces"
-printf "auto lo\niface lo inet loopback \n\nauto ${1}\niface ${1} inet static\n address ${2}\ngateway 192.168.0.1\nnetmask 255.255.255.0\ndns-nameservers 8.8.8.8\n " | sudo tee -a /etc/network/interfaces
+printf "auto lo\niface lo inet loopback \n\nauto ${1}\niface ${1} inet static\n  address ${2}\n  gateway 192.168.0.1\n  netmask 255.255.255.0\n  dns-nameservers 8.8.8.8\n " | sudo tee /etc/network/interfaces
 printf "\nPlease reboot or restart networking..."
 echo "New local ip address will be ${1}..."
