975ac7dce430a98cdea016f8cf7d7120b2869bd0
Michael Prather <mprather1@gmail.com>
2016-07-21 21:36:32 -0500

    refactored create bash aliases

--- run/bash_aliases.sh
+++ run/bash_aliases.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
 
 create_bash_aliases(){
+
+  if $basic = true
+    then
+      installation_type="basic"
+    else
+      installation_type="advanced"
+  fi
+
   touch ~/.bash_aliases
   cat $(directory)/aliases.txt > ~/.bash_aliases
 
   printf "alias spyware_update='cd $(directory) && git pull origin master'\n" >> ~/.bash_aliases
 
-  scripts=($(directory)/advanced/*/*.sh)
-  for script in "${scripts[@]}"; do
-    filename="/${script#*/}"
-    alias_filename="${script##*/}"
-    fwe="${alias_filename%.*}"
-    file_path=$(printf "${filename}" | sed 's/\..\///g')
-    printf "alias ${fwe}='bash $file_path'\n" >> ~/.bash_aliases
-  done
-}
-
-create_basic_aliases(){
-  touch ~/.bash_aliases
-  cat $(directory)/aliases.txt > ~/.bash_aliases
-
-  printf "alias spyware_update='cd $(directory) && git pull origin master'\n" >> ~/.bash_aliases
-
-  scripts=($(directory)/basic/*/*.sh)
+  scripts=($(directory)/$installation_type/*/*.sh)
   for script in "${scripts[@]}"; do
     filename="/${script#*/}"
     alias_filename="${script##*/}"
