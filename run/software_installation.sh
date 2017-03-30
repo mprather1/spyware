557a9580be05e55ddf4a90834bcfd32df477bec6
Michael Prather <mprather1@gmail.com>
2017-03-30 23:20:11 +0000

    refactored software installation

--- run/software_installation.sh
+++ run/software_installation.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env bash
 
 install_software(){
+  get_software_list
+  pre_install
   sudo apt-get install -y curl
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   sudo apt-get update && \
   sudo apt-get install $new_software -y
   sudo dpkg -i $(directory)/misc/synergy.deb $(directory)/misc/xscreensaver.deb
   bash $(directory)/misc/npm.sh
-  case $software_type in 
-    "rpi")
-      printf "No raspi ruby yet...\n"
-    ;;
-    *)
-      echo "gem: --no-document" >> ~/.gemrc
-      bash $(directory)/misc/ruby_gems.sh
-    ;;
-  esac
-  sudo apt-get upgrade -y
+  post_install
+  sudo apt-get upgrade -y  
 }
 
-get_software_list(){
+pre_install(){
   case $software_type in
     "rpi")
       curl -sSL https://get.docker.com | sh
@@ -29,8 +23,22 @@ get_software_list(){
       sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
       sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
     ;;
+  esac  
+}
+
+post_install(){
+  case $software_type in 
+    "rpi")
+      printf "No raspi ruby yet...\n"
+    ;;
+    *)
+      echo "gem: --no-document" >> ~/.gemrc
+      bash $(directory)/misc/ruby_gems.sh
+    ;;
   esac
-  
+}
+
+get_software_list(){
   repositories=$(directory)/run/software_lists/${software_type}/repos.txt
   readarray repos < $repositories
     for repo in "${repos[@]}"; do
