5f52024a551c55da6552e17fdcbd2c6a4f27a510
Michael Prather <mprather1@users.noreply.github.com>
2017-04-07 12:43:18 -0500

    Removed optparse from ruby gems

--- misc/ruby_gems.sh
+++ misc/ruby_gems.sh
@@ -1,1 +1,1 @@
-sudo gem install bundler nokogiri rails rspec rubyzip redis faker lolcat net-ssh colorize optparse daemons execjs rest-client slop pry highline excon pg httparty formatador rb-inotify
+sudo gem install bundler nokogiri rails rspec rubyzip redis faker lolcat net-ssh colorize daemons execjs rest-client slop pry highline excon pg httparty formatador rb-inotify # optparse
