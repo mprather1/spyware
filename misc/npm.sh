da94aedfae221b63c163811de7929898c66e653d
Michael Prather <mprather1@users.noreply.github.com>
2017-04-09 11:35:25 -0500

    Removed feathers-cli

--- misc/npm.sh
+++ misc/npm.sh
@@ -1,1 +1,1 @@
-sudo npm install -g browserify forever http-server bower mocha faker webpack webpack-dev-server express express-generator knex node-mongo-seeds koa pm2 nodemon lolcatjs npm-check feathers-cli # standard
+sudo npm install -g browserify forever http-server bower mocha faker webpack webpack-dev-server express express-generator knex node-mongo-seeds koa pm2 nodemon lolcatjs npm-check # feathers-cli # standard
