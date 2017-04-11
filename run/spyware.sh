460686e5f651430322303488668d9877cf5c3155
Michael Prather <mprather1@gmail.com>
2017-04-11 17:45:05 +0000

    tmux.conf in run/spyware.sh

--- run/spyware.sh
+++ run/spyware.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 install_spyware(){
+  user=$(whoami)
+  
+  if [ ! -f $(directory)/.tmux.conf ]; then
+    cat $(directory)/tmux.conf.txt > /home/$user/.tmux.conf
+  fi
+  
   if [ $software_type = "desktop" ]; then
     sudo cp -rv $(directory)/themes/* /usr/share/themes
     sudo cp -rv $(directory)/wallpaper/* /usr/share/xfce4/backdrops    
    
     touch ~/.gtkrc-2.0
-    cat $(directory)/gtkrc.txt > ~/.gtkrc-2.0
+    cat $(directory)/gtkrc.txt > /home/$user/.gtkrc-2.0
   fi
 }
