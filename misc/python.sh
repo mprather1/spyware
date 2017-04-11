2a1b9670fdb065670486fc9d73a2c9b533aabbb3
Michael Prather <mprather1@gmail.com>
2017-04-11 21:24:21 +0000

    added some python packages to python.sh

--- misc/python.sh
+++ misc/python.sh
@@ -1,5 +1,7 @@
 pip install requests prettytable progressbar flask uuid
 
+pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose bokeh
+
 pip3 install sanic asyncpg
 
 sudo -H pip install flask arrow cython
