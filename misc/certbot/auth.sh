6166f5e1acc59a9b40c8d3cec8e3f9e89ccd9d39
Michael Prather <mprather1@gmail.com>
2017-05-27 20:29:18 +0000

    update certbot

--- misc/certbot/auth.sh
+++ misc/certbot/auth.sh
@@ -1,17 +1,13 @@
-chkarg(){
-  if [ ! -z ${1+x} ]; then
-    return 0
-  else
-    return 1
-  fi
-}
+source $(dirname $0)/../../data.sh;
 
 if chkarg $2; then
-  sudo apt-get install software-properties-common -y && \
-  sudo add-apt-repository ppa:certbot/certbot -y && \
-  sudo apt-get update && \
-  sudo apt-get install certbot -y && \ 
-  certbot certonly --standalone --agree-tos -n -d $1 --email $2 
+  if repo_not_installed ppa:certbot/certbot; then
+    sudo apt-get install software-properties-common -y && \
+    sudo add-apt-repository ppa:certbot/certbot -y && \
+    sudo apt-get update && \
+    sudo apt-get install certbot -y && \ 
+    sudo certbot certonly --standalone --agree-tos -n -d $1 --email $2   
+  fi
 else
   printf "Input Error...\n./auth.sh <domain> <email>"
 fi
