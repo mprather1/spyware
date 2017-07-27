46dd12f05b0d77715f5730ffb79e133cccc4f063
Michael Prather <mprather1@gmail.com>
2017-07-27 23:45:38 +0000

    dammit

--- bin/spyware_update/spyware_update.sh
+++ bin/spyware_update/spyware_update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source $(dirname $0)/../../utilities.sh
-source $(directory)\bin/git/git_update.sh
+source $(directory)/bin/git/git_update.sh
 
 spyware_update(){
   printf 'updating master git repo...\n'
