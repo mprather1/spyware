e444e027003b438a9804b7f896b5d219ea87c79f
Michael Prather <mprather1@gmail.com>
2017-07-28 15:56:55 +0000

    2.4.2

--- bin/git/git_push.sh
+++ bin/git/git_push.sh
@@ -3,6 +3,5 @@ source $(dirname $0)/../../utilities.sh
 source $(dirname $0)/../bin/update-version/index.js
 
 git_push(){
-  git push -u origin master && git push --tags && update-version
-  printf "\nAll Done!!\n"
+  git push -u origin master && git push --tags
 }
--- bin/git/git_update.sh
+++ bin/git/git_update.sh
@@ -28,6 +28,6 @@ git_update(){
 add_to_path(){
   bash $(pwd)${directory}/bin/update-version/create.sh
   cp $(directory)/bin/update-version/update-version.default $(directory)/bin/update-version/update-version
-  printf "\n/usr/bin/env bash $(pwd)${directory}/bin/update-version/script.sh \$1 \$2" >> $(directory)/bin/update-version/update-version
+  printf "\n/usr/bin/env bash $(pwd)${directory}/bin/update-version/script.sh \$(pwd) \$1" >> $(directory)/bin/update-version/update-version
   sudo cp $(directory)/bin/update-version/update-version /usr/local/bin/update-version
 }
