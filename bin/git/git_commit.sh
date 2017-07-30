ef7ac1fe70dbda228994352655aa10c43b7c9c9d
Michael Prather <mprather1@gmail.com>
2017-07-30 17:37:32 +0000

    update gitp to use npm-updater

--- bin/git/git_commit.sh
+++ bin/git/git_commit.sh
@@ -6,9 +6,9 @@ _dir=$(pwd)
 git_commit(){
   if [ -f $_dir/package.json ]
     then 
-      standard && npm-updater $2 &&  git add -A && git commit -m "${1}" && git tag $2
+      standard && npm-updater $2 && git add -A && git commit -m "${1}" && git tag $2
     else 
-      update-version $2 && git add -A && git commit -m "${1}" && git tag $2
+      npm-updater $2 && git add -A && git commit -m "${1}" && git tag $2
   fi
   
   printf "\nAll Done!!!\n"
