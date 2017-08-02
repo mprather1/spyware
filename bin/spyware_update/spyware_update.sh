2c9ea883c863103ad78942b8a2e3c539677cf1c2
Michael Prather <mprather1@gmail.com>
2017-08-02 17:26:37 +0000

    update

--- bin/spyware_update/spyware_update.sh
+++ bin/spyware_update/spyware_update.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 source $(dirname $0)/../../utilities.sh
 source $(directory)/bin/git/git_update.sh
+source $(directory)/run/get_installer.sh
 
 spyware_update(){
   printf 'updating master git repo...\n'
   git -C $(directory) pull origin master && \
 
   git_update
+  install_js
 }
