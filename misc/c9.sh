ac65157d02e970f4ba87ae65982c588bc8e76c30
Michael Prather <mprather1@gmail.com>
2017-11-06 02:12:57 +0000

    2.7.10-script-for-c9

---
+++ misc/c9.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+user=$(whoami)
+
+if [ ! -d /home/$user/Development ]; then
+  mkdir /home/$user/Development
+fi
+
+git clone git://github.com/c9/core.git /home/$user/c9sdk
+/home/$user/c9sdk/scripts/install-sdk.sh
+printf "alias cloud9='pm2 start /home/$user/c9sdk/server.js -x -- -p 8080 -a ${1}:${2} --listen localhost -w /home/$user/Development'\n" >> /home/$user/.bash_aliases
