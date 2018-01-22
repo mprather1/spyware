4a0c570ea7021a95bd8f5d9da0cfc04cca8f0ef2
Michael Prather <mprather1@gmail.com>
2018-01-22 01:42:30 +0000

    removed git update from gitp.sh

--- bin/spyware_update/spyware_update.sh
+++ bin/spyware_update/spyware_update.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 source $(dirname $0)/../../utilities.sh
-source $(directory)/bin/git/git_update.sh
 source $(directory)/run/get_installer.sh
 
 spyware_update(){
   printf "$(random_color)Updating master${NC}...\n"
   git -C $(directory) pull origin master && \
 
-  git_update
   install_js
 }
