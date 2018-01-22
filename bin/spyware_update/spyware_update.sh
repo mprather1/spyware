c7653eb5d93f91603175941338ce2c41a018bda4
Michael Prather <mprather1@gmail.com>
2018-01-22 03:50:44 +0000

    fixed spyware update

--- bin/spyware_update/spyware_update.sh
+++ bin/spyware_update/spyware_update.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 source $(dirname $0)/../../utilities.sh
-source $(directory)/run/get_installer.sh
 
 spyware_update(){
   printf "$(random_color)Updating master${NC}...\n"
   git -C $(directory) pull origin master && \
 
-  install_js
+  git_update
 }
