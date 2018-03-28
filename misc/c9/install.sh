a27a1e0365729116154690f21c9c83741007b32b
Michael Prather <mike@skynet.shintech.ninja>
2018-03-28 06:28:24 +0000

    misc

--- misc/c9/install.sh
+++ misc/c9/install.sh
@@ -7,5 +7,7 @@ if [ ! -d /home/$user/Development ]; then
 fi
 
 git clone git://github.com/c9/core.git /home/$user/c9sdk
+
 /home/$user/c9sdk/scripts/install-sdk.sh
+
 printf "alias cloud9='pm2 start /home/$user/c9sdk/server.js -x -- -p 8080 -a ${1}:${2} --listen localhost -w /home/$user/Development'\n" >> /home/$user/.bash_aliases
--- misc/certbot/auth.sh
+++ misc/certbot/auth.sh
@@ -1,5 +1,11 @@
-sudo apt-get install software-properties-common -y && \
-sudo add-apt-repository ppa:certbot/certbot -y && \
-sudo apt-get update && \
-sudo apt-get install certbot -y && \ 
-sudo certbot certonly --standalone --agree-tos -n -d $1 --email $2   
+#!/usr/bin/env bash
+
+if [ ! -z ${2+x} ]; then
+  sudo apt-get install software-properties-common -y && \
+  sudo add-apt-repository ppa:certbot/certbot -y && \
+  sudo apt-get update && \
+  sudo apt-get install certbot -y && \ 
+  sudo certbot certonly --standalone --agree-tos -n -d $1 --email $2  
+else
+  printf "Usage -> ./auth.sh <domain> <email>\n"
+fi
