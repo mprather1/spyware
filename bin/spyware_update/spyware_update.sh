5db347692eb7e2bf52cfcc2c3576cb57b52a2a6e
Michael Prather <mprather1@gmail.com>
2018-08-30 01:49:38 +0000

    comment out git update in /bin/spyware_update/spyware_update.sh

--- bin/spyware_update/spyware_update.sh
+++ bin/spyware_update/spyware_update.sh
@@ -3,7 +3,7 @@ source $(dirname $0)/../../utilities.sh
 
 spyware_update(){
   printf "$(random_color)Updating master${NC}...\n"
-  git -C $(directory) pull origin master && \
+  git -C $(directory) pull origin master
 
-  git_update
+  # git_update
 }
