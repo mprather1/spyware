4dfecb3ef700746d14cf32ddd010045c116d7b00
Michael Prather <mprather1@gmail.com>
2018-09-05 02:19:13 +0000

    add nodemon to misc/npm.sh

--- misc/npm.sh
+++ misc/npm.sh
@@ -1,2 +1,2 @@
 # sudo npm install -g forever http-server mocha faker webpack webpack-dev-server express  knex node-mongo-seeds koa pm2 nodemon lolcatjs npm-check standard
-sudo npm install -g pm2 standard
+sudo npm install -g pm2 standard nodemon
