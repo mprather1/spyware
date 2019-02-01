b1488487e2d82eb8655dbcebfd2fd35f71f4ff30
Michael Prather <mprather1@gmail.com>
2019-02-01 12:19:13 +0000

    added title to notification

--- bin/notify/notify.sh
+++ bin/notify/notify.sh
@@ -5,7 +5,7 @@ notify(){
   check=$(echo "$XDG_DATA_DIRS" | grep -Eo 'xfce|kde|gnome')
 
   if [ ! -z "${check}" ]; then
-    notify-send "Command has finished!!!" -i $(directory)/icons/windows-128.png
+    notify-send "Spyware Notification" "Command has finished!!!" -i $(directory)/icons/windows-128.png
   else
     wall "Command has finished!!!"
   fi
