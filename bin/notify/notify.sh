b20282aea53d46444bf779d020326768171d42e1
Michael Prather <mprather1@gmail.com>
2019-02-01 11:52:09 +0000

    fixed

--- bin/notify/notify.sh
+++ bin/notify/notify.sh
@@ -3,7 +3,7 @@ source $(dirname $0)/../../utilities.sh
 
 notify(){
   if chkarg $1; then 
-    $(command) $1 && notify-send -i $(directory)/icons/windows-128.png "${1} has finished!!!"
+    $(command) $1 && notify-send "${1} has finished!!!" -i $(directory)/icons/windows-128.png
   else
     notify-send -i $(directory)/icons/windows-128.png 'Command has finished!!!'
   fi
