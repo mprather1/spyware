19dee2c0186a0e19e9430a9c0c19cbcec5579304
Michael Prather <mprather1@gmail.com>
2019-02-08 09:44:39 +0000

    prevent hashing of hosts in .ssh/known_hosts with script in run/ssh_config.sh

--- run/ssh_config.sh
+++ run/ssh_config.sh
@@ -8,11 +8,14 @@ ssh_config(){
       printf "Creating key and adding to agent...\n"
       ssh-keygen -t rsa -b 4096 -C "${email}" -f $HOME/.ssh/id_rsa -q -N ""
       eval "$(ssh-agent -s)"
-      
+
+      printf 'Editing .ssh/config\n'
+      printf "Host *\n\tHashKnownHosts no" > $HOME/.ssh/config
+
       ssh-add /home/$user/.ssh/id_rsa
       printf "Done!!\n"
     else
       printf "key already exists...\n"
       printf "skipping...\n"
-  fi  
+  fi
 }
