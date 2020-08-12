401119049e121f5026a8f4cb3c523b9cece8f8ea
Michael Prather <mprather1@gmail.com>
2020-08-12 05:35:10 +0000

    copy all images to pixmaps dir

--- run/spyware.sh
+++ run/spyware.sh
@@ -10,6 +10,7 @@ install_spyware(){
   if [ -z $software_type ] && [ "${software_type}" == "desktop" ]; then
     sudo cp -rv $(directory)/themes/* /usr/share/themes/
     sudo cp -rv $(directory)/wallpaper/* /usr/share/xfce4/backdrops/    
+    sudo cp -rv $(directory)/icons/* /usr/share/pixmaps/
     sudo cp -rv $(directory)/icons/svg/* /usr/share/icons/
   
     touch /home/$user/.gtkrc-2.0
